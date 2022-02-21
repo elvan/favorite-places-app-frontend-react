5947902ad53202e19fbf101551ab9074761bbee5
Elvan H <elvanhd@gmail.com>
2022-02-21 08:26:15 +0700

    adding a portal for the side-drawer

--- frontend/src/shared/components/navigation/SideDrawer.js
+++ frontend/src/shared/components/navigation/SideDrawer.js
@@ -1,5 +1,9 @@
+import ReactDOM from 'react-dom';
 import './SideDrawer.css';
 
 export const SideDrawer = (props) => {
-  return <aside className='side-drawer'>{props.children}</aside>;
+  const content = <aside className='side-drawer'>{props.children}</aside>;
+  const drawerRoot = document.getElementById('drawer-root');
+
+  return drawerRoot ? ReactDOM.createPortal(content, drawerRoot) : null;
 };
