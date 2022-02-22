d9acaf6ead30406b52946a48f892bb4712e7f5af
Elvan H <elvanhd@gmail.com>
2022-02-22 10:05:05 +0700

    adding a custom form input component

--- src/places/pages/PlaceAddPage.js
+++ src/places/pages/PlaceAddPage.js
@@ -1,7 +1,18 @@
+import Input from '../../shared/components/forms/Input';
+import './PlaceAddPage.css';
+
 export const PlaceAddPage = () => {
   return (
-    <div>
-      <h1>PlaceAddPage</h1>
-    </div>
+    <form className='place-form'>
+      <Input
+        element='input'
+        id='title'
+        label='Title'
+        type='text'
+        placeholder='Place Title'
+        validators={[]}
+        onChange={() => {}}
+      />
+    </form>
   );
 };
