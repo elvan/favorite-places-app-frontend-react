7b4e685ebeddfd383de89758a61682f47f220bb1
Elvan H <elvanhd@gmail.com>
2022-02-23 03:17:54 +0700

    managing form-wide state

--- src/places/pages/PlaceAddPage.js
+++ src/places/pages/PlaceAddPage.js
@@ -1,4 +1,5 @@
-import { useCallback } from 'react';
+import { useCallback, useReducer } from 'react';
+import { Button } from '../../shared/components/forms/Button';
 import { Input } from '../../shared/components/forms/Input';
 import {
   VALIDATOR_MINLENGTH,
@@ -6,10 +7,61 @@ import {
 } from '../../shared/util/validators';
 import './PlaceAddPage.css';
 
-export const PlaceAddPage = () => {
-  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: {
+            value: action.value,
+            isValid: action.isValid,
+          },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
 
-  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+export const PlaceAddPage = () => {
+  // @ts-ignore
+  const [state, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: '',
+        isValid: false,
+      },
+      description: {
+        value: '',
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
+  const inputHandler = useCallback(
+    (id, value, isValid) => {
+      // @ts-ignore
+      dispatch({
+        type: 'INPUT_CHANGE',
+        inputId: id,
+        value: value,
+        isValid: isValid,
+      });
+    },
+    [dispatch]
+  );
 
   return (
     <form className='place-form'>
@@ -20,7 +72,7 @@ export const PlaceAddPage = () => {
         type='text'
         errorText='Please enter a valid title'
         validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
-        onInput={titleInputHandler}
+        onInput={inputHandler}
       />
       <Input
         id='description'
@@ -28,8 +80,11 @@ export const PlaceAddPage = () => {
         type='textarea'
         errorText='Please enter a valid description'
         validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
-        onInput={descriptionInputHandler}
+        onInput={inputHandler}
       />
+      <Button type='submit' disabled={!state.isValid}>
+        ADD PLACE
+      </Button>
     </form>
   );
 };
