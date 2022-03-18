1666c031ea1fd9634dc241ce014454b1707f86ec
Elvan H <elvanhd@gmail.com>
2022-03-18 17:32:58 +0700

    cleaning up the code

--- src/shared/components/navigation/MainNavigation.js
+++ src/shared/components/navigation/MainNavigation.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import Backdrop from '../UIElements/Backdrop';
 import MainHeader from './MainHeader';
+import './MainNavigation.css';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
-import Backdrop from '../UIElements/Backdrop';
-import './MainNavigation.css';
 
-const MainNavigation = props => {
+const MainNavigation = () => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
   const openDrawerHandler = () => {
@@ -19,31 +18,32 @@ const MainNavigation = props => {
   };
 
   return (
-    <React.Fragment>
+    <>
       {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+
       <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
-        <nav className="main-navigation__drawer-nav">
+        <nav className='main-navigation__drawer-nav'>
           <NavLinks />
         </nav>
       </SideDrawer>
 
       <MainHeader>
         <button
-          className="main-navigation__menu-btn"
+          className='main-navigation__menu-btn'
           onClick={openDrawerHandler}
         >
           <span />
           <span />
           <span />
         </button>
-        <h1 className="main-navigation__title">
-          <Link to="/">YourPlaces</Link>
+        <h1 className='main-navigation__title'>
+          <Link to='/'>FavoritePlaces</Link>
         </h1>
-        <nav className="main-navigation__header-nav">
+        <nav className='main-navigation__header-nav'>
           <NavLinks />
         </nav>
       </MainHeader>
-    </React.Fragment>
+    </>
   );
 };
 
